8dbdca384f16f3e56d271b3bd754643ab9885a4f
常晓阳 <cnxzcxy@gmail.com>
2019-03-05 10:29:41 +0800

    删除无用的接口

--- src/NotifyServant.php
+++ src/NotifyServant.php
@@ -35,56 +35,18 @@ class NotifyServant {
         $this->_iTimeout = empty($config->getAsyncInvokeTimeout()) ? 2000 : $config->getAsyncInvokeTimeout();
     }
 
-	/**
-	 * @param string $sServerName 
-	 * @param string $sThreadId 
-	 * @param string $sMessage 
-	 * @return void
-	 */
-	public function reportServer($sServerName,$sThreadId,$sMessage){
-
-    }
-	/**
-	 * @param string $sServerName 
-	 * @param int $level 
-	 * @param string $sMessage 
-	 * @return void
-	 */
-	public function notifyServer($sServerName,$level,$sMessage){
+	public function reportNotifyInfo(ReportInfo $info){
         $requestPacket = new RequestPacket();
         $requestPacket->_iVersion = $this->_iVersion;
         $requestPacket->_funcName = __FUNCTION__;
         $requestPacket->_servantName = $this->_servantName;
         $encodeBufs = [];
-        $__buffer = TUPAPIWrapper::putString('sServerName', 1, $sServerName, $this->_iVersion);
-        $encodeBufs['sServerName'] = $__buffer;
-        $__buffer = TUPAPIWrapper::putString('level', 2, $level, $this->_iVersion);
-        $encodeBufs['level'] = $__buffer;
-        $__buffer = TUPAPIWrapper::putString('sMessage', 3, $sMessage, $this->_iVersion);
-        $encodeBufs['sMessage'] = $__buffer;
-        $buffer_vec = new \TARS_Vector(\TARS::STRING);
-        foreach ($buffer as $singlebuffer) {
-            $buffer_vec->pushBack($singlebuffer);
-        }
-        $__buffer = TUPAPIWrapper::putVector('buffer', 4, $buffer_vec, $this->_iVersion);
-        $encodeBufs['buffer'] = $__buffer;
+
+        $__buffer = TUPAPIWrapper::putStruct('info', 1, $info, $this->_iVersion);
+        $encodeBufs['info'] = $__buffer;
         $requestPacket->_encodeBufs = $encodeBufs;
         $sBuffer = $this->_communicator->invoke($requestPacket, $this->_iTimeout);
 
-    }
-	/**
-	 * @param struct $stKey \Tars/notify\gc\memberrpc\obj\classes\NotifyKey
-	 * @param struct $stInfo \Tars/notify\gc\memberrpc\obj\classes\NotifyInfo =out=
-	 * @return int
-	 */
-	public function getNotifyInfo(NotifyKey $stKey,NotifyInfo &$stInfo){
-
-    }
-	/**
-	 * @param struct $info \Tars/notify\gc\memberrpc\obj\classes\ReportInfo
-	 * @return void
-	 */
-	public function reportNotifyInfo(ReportInfo $info){
 
     }
 }
